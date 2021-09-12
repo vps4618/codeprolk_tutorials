8ddfc5b4896a404266e08451aecd408cf2fd2a16
B.D.Vishwa Praveen Sarathchandra <88998540+VishwaPraveen4618@users.noreply.github.com>
2021-09-12 20:27:05 +0530

    Delete python_exercises directory

--- python_exercises/python_exercises_episode_1.py
+++
@@ -1,9 +0,0 @@
-def generator(sentence):
-    word_list=sentence.split()
-    short_form=''
-    for i in word_list:
-        short_form=short_form+i[0].upper()
-    print(short_form)    
-
-text=input("Enter your sentence : ")
-generator(text)
